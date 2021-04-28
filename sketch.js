fab033a1c3de67a09151725455022384921797c2
Shally Mudgal <shally.ps1890@gmail.com>
2021-04-28 13:00:19 +0530

    Modified

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +38,10 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    constrainedLog=new Log(100,100,100,PI/2);
+
+   chain=new Chain(bird.body,constrainedLog.body);
+
 }
 
 function draw(){
@@ -61,4 +67,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    constrainedLog.display();
+   
+     chain.display();
 }
